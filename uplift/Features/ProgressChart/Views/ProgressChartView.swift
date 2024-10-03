65ef0c853a61c08be4330f7ea668e00ae80f945f
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-02 23:35:09 -0400

    slight change to progress chart appearance

--- uplift/Features/ProgressChart/Views/ProgressChartView.swift
+++ uplift/Features/ProgressChart/Views/ProgressChartView.swift
@@ -27,20 +27,22 @@ struct ProgressChartView: View {
             HStack(alignment: .bottom, spacing: 0){
                 ForEach(viewModel.weeklyProgress.reversed(), id: \.weekStartDate) {week in
                     VStack(alignment: .center, spacing: 4) {
-                        VStack(spacing: 2){
-                            ForEach(0..<week.recoveryCount, id: \.self) { _ in
-                                RoundedRectangle(cornerRadius: 2)
-                                    .fill(Color("Blue"))
-                                    .frame(width: 16, height: 20)
+                        VStack(spacing:0){
+                            VStack(spacing: 2){
+                                ForEach(0..<week.recoveryCount, id: \.self) { _ in
+                                    RoundedRectangle(cornerRadius: 2)
+                                        .fill(Color("Blue"))
+                                        .frame(width: 16, height: 20)
+                                }
+                                ForEach(0..<week.workoutCount, id: \.self) { _ in
+                                    RoundedRectangle(cornerRadius: 2)
+                                        .fill(Color("Orange"))
+                                        .frame(width: 16, height: 20)
+                                }
                             }
-                            ForEach(0..<week.workoutCount, id: \.self) { _ in
-                                RoundedRectangle(cornerRadius: 2)
-                                    .fill(Color("Orange"))
-                                    .frame(width: 16, height: 20)
-                            }
+                            Rectangle()
+                                .frame(width: 50, height: 2)
                         }
-                        Rectangle()
-                            .frame(width: 50, height: 2)
                         Text(week.weekStartDate)
                             .font(.system(size: 12))
                     }
