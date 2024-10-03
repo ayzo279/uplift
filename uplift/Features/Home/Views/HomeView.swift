b2401a2923b16572e01daa3ce22a186d39895f3a
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-02 23:31:13 -0400

    weekly progress chart built

--- uplift/Features/Home/Views/HomeView.swift
+++ uplift/Features/Home/Views/HomeView.swift
@@ -9,14 +9,28 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack{
+        VStack(spacing:0){
             HomeHeaderView()
             ScrollView{
+                Rectangle()
+                    .fill(Color("lightGray"))
+                    .frame(width:.infinity, height: 4)
                 DailyProgressView()
+                Rectangle()
+                    .fill(Color("lightGray"))
+                    .frame(width:.infinity, height: 4)
+                ProgressChartView()
+                Rectangle()
+                    .fill(Color("lightGray"))
+                    .frame(width:.infinity, height: 4)
             }
+            .padding(.vertical, -10)
+            .frame(height: 600)
             TabBarView()
         }
         .navigationBarBackButtonHidden(true)
+        .background(Color("White"))
+        
     }
 }
 
